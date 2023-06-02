7210f81fd9ff03a4596ea0b8441c7afc326d9a20
imnotvargas <125502308+imnotvargas@users.noreply.github.com>
2023-06-02 13:21:38 -0600

    Evaluacion1

---
+++ EJERCICIOS/Evaluacion.java
@@ -0,0 +1,6 @@
+public class Evaluacion {
+    public static void main(String[]args) {
+    //El método main ejecuta las tareas de la clase como un comentario de una sola línea.
+    System.out.println("¡Mi primer programa Java desde cero!");   
+    }
+}
