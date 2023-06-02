9c701e8fcac05d3396aa1b1b5f0d84e1348906fe
imnotvargas <125502308+imnotvargas@users.noreply.github.com>
2023-06-02 13:33:52 -0600

    Add files via upload

---
+++ EJERCICIOS/Creador.java
@@ -0,0 +1,14 @@
+public class Creador{
+    public static void main(String[]args) {
+        String nombre;
+        int anio;
+
+        nombre = "James Gosling";
+        anio = 1995;
+
+        System.out.println("Salida:");   
+        System.out.println(nombre);   
+        System.out.println(anio);   
+
+    }
+}
